8184d44699b05fc261451af19d98ea57748480b6
windows11vpsbilal <109600131+windows11vpsbilal@users.noreply.github.com>
2022-07-19 17:03:52 +0300

    Update windows11.sh

--- windows11.sh
+++ windows11.sh
@@ -8,7 +8,7 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o windows.iso https://software.download.prss.microsoft.com/dbazure/Win11_Turkish_x64v1.iso?t=3db05016-a4ba-4d63-b4b8-912b414fa4f3&e=1658321352&h=fd235a460cece5b346b8d82b8b8c4744f1c7a7220dd816ef42ffe871f0c9e009
+curl -L -o windows.iso https://app.vagrantup.com/bilalvps65/boxes/bilalvps/versions/1.1.3/providers/qemu.box
 curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
 echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
